0aa710c84443d43b87fc0223bd447a5ab92f9265
SathyaChilumula <sp.satyadeveloper@gmail.com>
2019-02-27 06:32:53 +0000

    single_multiple_variable_python

---
+++ python/scripts/python_single_variable.py
@@ -0,0 +1,12 @@
+# How to write a single variable in python program
+a = 1
+name = 'Priya'
+salary = 50000
+print (a)
+print (name)
+print (salary)
+
+x,y,z = 10,20,30
+print (y,z)
+aws,azure,gcp = 'Route53','Kubernetes','Docker'
+print (aws,azure,gcp)
